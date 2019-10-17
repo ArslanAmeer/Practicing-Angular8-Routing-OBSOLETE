786c6f544ec51c28a075111e07e4ce5286c4f4b9
ArslanAmeer <arslan.ameer@hotmail.com>
2019-10-17 13:10:22 +0500

    ✨👍 Update: Routing Programmatically in TS files
    just Like redirecting to some other page we can redirect with injecting Router in any TS file

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  loadRouter() {
+    // Just Like redirecting to some other page we can redirect with injecting Router in any TS file
+    this.router.navigate(['servers']);
   }
-
 }
