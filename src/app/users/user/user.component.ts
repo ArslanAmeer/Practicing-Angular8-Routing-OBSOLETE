ceb2227e7aeb6aa665e122333447943a8168f422
ArslanAmeer <arslan.ameer@hotmail.com>
2019-10-18 13:05:42 +0500

    ✨👍 Update: Unsubscribing Params subscription
    To Destroy or remove Subscription on Component Destroy

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs'; // For Subscrition of parameters
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: { id: number; name: string };
+  // A variable holding subscription to Destroy or remove Subscription on Component Destroy later
+  paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) {}
 
@@ -20,9 +23,14 @@ export class UserComponent implements OnInit {
     };
 
     // This is to subscribe to paramaters change when remain on same component
-    this.route.params.subscribe((params: Params) => {
+    this.paramsSubscription = this.route.params.subscribe((params: Params) => {
       this.user.id = params['id'];
       this.user.name = params['name'];
     });
+  }
+
+  // TO Destroy or remove Subscription on Component Destroy
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
   }
 }
