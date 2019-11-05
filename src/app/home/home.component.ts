ed9ddb1c2d5d93f7612864313981363fe4bd5a1d
ArslanAmeer <arslan.ameer@hotmail.com>
2019-11-05 15:12:25 +0500

    ✨ Add: Testing guard with Login button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthService} from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,11 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
   loadRouter() {
     // Just Like redirecting to some other page we can redirect with injecting Router in any TS file
@@ -19,5 +22,14 @@ export class HomeComponent implements OnInit {
   loadServer(id: number) {
     // testing passing QueryParams & fragment programmatically
     this.router.navigate(['servers', id, 'edit'], {queryParams: {allowGet: '2'}, fragment: 'loading2'});
+  }
+
+  // For testing Guard, if loggedIn=true it will allow to open servers, if loggedIn=false it will not
+  login() {
+    this.authService.login();
+  }
+
+  logout() {
+    this.authService.logout();
   }
 }
